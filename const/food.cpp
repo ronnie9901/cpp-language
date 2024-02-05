f65df0d9772b665a2f8277c15c5d5050a83436c0
rohan nayak <148763509+ronnie9901@users.noreply.github.com>
2024-02-05 13:39:50 +0530

    Add files via upload

---
+++ const/food.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+class food 
+{
+	int id ,rating,staffq,year ;
+	static int i;
+	string name,location,type; 
+	public:
+	
+	food()
+	{
+	   cout<< " welcome to my cafe"<<endl;
+	}
+	 void input(int x)
+	 {
+	 	cout<<" enter the details : "<<x<<endl;
+	 	cin>>id;
+		cin.ignore();
+		
+		cout<< " name ";
+		getline(cin,name);
+		
+		cout<<" staff quantity ";
+		cin>>id;
+		cin.ignore();
+		
+		cout<<" rating ";
+		cin>>id;
+		cin.ignore();
+		
+		cout<<" year ";
+		cin>>id;
+		cin.ignore();
+		
+		cout<< " location ";
+		getline(cin,location);
+		
+		cout<< " type ";
+		getline(cin,type);
+	 }
+	
+	void output()
+	{   
+	    cout<<"ente the details"<<endl;
+	    cout<< "-------------------------"<<endl;
+		cout<< " enter id:"<<id<<endl;
+		cout<< " enter name:"<<name<<endl;
+		cout<< " enter rating:"<<rating<<endl;
+		cout<< " enter staffq:"<<staffq<<endl;
+		cout<< " enter year:"<<year<<endl;
+		cout<< " enter location:"<<location<<endl;
+		cout<< " enter type:"<<type<<endl;
+	}
+};
+main()
+{
+	class food f[2];
+	
+	int x=1,i;
+	for(i=0;i<2;i++)
+	{
+		f[i].input(x);
+		
+		x++;
+	
+	}
+	for(i=0;i<2;i++)
+	{
+		f[i].output();
+	}
+	
+	
+}
+
